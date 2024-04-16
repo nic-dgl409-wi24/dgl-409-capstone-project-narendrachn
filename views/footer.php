e189385e9fdf5e3dec702e008e83e0a1a5b67fbb
narendrachn <109662555+narendrachn@users.noreply.github.com>
2024-04-16 13:45:14 -0700

    Add content to footer php file

---
+++ views/footer.php
@@ -0,0 +1,17 @@
+<footer>
+        <div class="social-links">
+            <a href="https://www.facebook.com/" target="_blank"><img src="facebook-icon.png"
+                    alt="Facebook"></a>
+            <a href="https://www.instagram.com/" target="_blank"><img src="instagram-icon.png"
+                    alt="Instagram"></a>
+            <a href="https://www.twitter.com/yourDriverConnectPage" target="_blank"><img src="twitter-icon.png"
+                    alt="Twitter"></a>
+        </div>
+        <p>&copy; 2024 Driver Connect. All rights reserved.</p>
+    
+    </footer>
+    <script src="script.js"></script>
+
+</body>
+
+</html>
