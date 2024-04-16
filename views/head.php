cf064f2781a0670e519f913d9f618826e2afef30
narendrachn <109662555+narendrachn@users.noreply.github.com>
2024-04-16 13:44:50 -0700

    Add content to head php file

---
+++ views/head.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Driver connect</title>
+
+    <script src="https://unpkg.com/openlayers@latest/dist/ol.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="https://unpkg.com/openlayers@latest/dist/ol.css" type="text/css">
+
+    <link rel="stylesheet" href="style.css">
+
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "lnwxcqk5ie");
+    </script>
+</head>
+
+<body>
+    <header class="header">
+        <h1>Driver Connect</h1>
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="direction.html">Directions</a></li>
+                <li><a href="notification.html">Live Traffic</a></li>
+                <li><a href="Locate-spare.html">Spare Parts Store</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="profile.html">Profile</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
